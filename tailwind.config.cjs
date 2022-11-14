9daafb74d652686886832f6f0d6ac25ebfcaef3c
Fabian Herrera Martinez <contactofabianmartinez@gmail.com>
2022-11-14 00:49:10 -0600

    🔁 CHANGE: little things changed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'footer': "url('/public/footer-bg.webp')",
+        'footer': "url('/public/Backgrounds/footer-bg.webp')",
       },
+      colors: {
+        'black-rgba': 'rgba(255, 255, 255, 0.05)',
+        'white-hsla': 'linear-gradient(153deg, hsla(0, 100%, 100%, 0.1), hsla(0, 0%, 100%, 0))',
+      }
     },
   },
   plugins: [],
